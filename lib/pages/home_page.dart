d60ec291023999f8268d98114fe13e8c70a8a887
Mahdinkh7 <mahdinkh7@gmail.com>
2024-02-15 01:40:08 +0330

    done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,115 +7,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: const Text("Daily Meditation"),
       ),
       body: ListView(
         padding: const EdgeInsets.all(14),
         children: [
           Center(child: Image.asset('assets/breath.png', width: 250)),
-          const SizedBox(height: 20),
-          Text(
-            "Catching your thoughts",
-            style: Theme.of(context).textTheme.titleMedium,
-            textAlign: TextAlign.center,
+          player(context),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            child: Text("Recommendations",
+                style: Theme.of(context).textTheme.titleMedium),
           ),
-          const SizedBox(height: 20),
-          Card(
-            elevation: 0.1,
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(CupertinoIcons.shuffle),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(CupertinoIcons.play),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(CupertinoIcons.heart),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  LinearProgressIndicator(
-                    value: 0.48,
-                    color: Colors.deepPurple,
-                    minHeight: 5,
-                    backgroundColor: Colors.grey.shade200,
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "01:56",
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                      Text(
-                        "03:00",
-                        style: Theme.of(context).textTheme.bodySmall,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            "Recommendations",
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
+          recommendations(context),
           const SizedBox(height: 5),
-          ...List.generate(4, (index) {
-            return Card(
-              color: Colors.white,
-              elevation: 0.1,
-              child: InkWell(
-                borderRadius: BorderRadius.circular(10),
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        'assets/breath_$index.png',
-                        width: 50,
-                      ),
-                      const SizedBox(width: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Focusing the mind",
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
-                          Text(
-                            "${index + 4} mins",
-                            style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.grey.shade600),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(CupertinoIcons.play_arrow),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          })
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -143,4 +51,113 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget player(context) {
+  return Column(children: [
+    const SizedBox(height: 20),
+    Text(
+      "Catching your thoughts",
+      style: Theme.of(context).textTheme.titleMedium,
+      textAlign: TextAlign.center,
+    ),
+    const SizedBox(height: 20),
+    Card(
+      elevation: 0.1,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.shuffle),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.play),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.heart),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            LinearProgressIndicator(
+              value: 0.48,
+              color: Colors.deepPurple,
+              minHeight: 5,
+              backgroundColor: Colors.grey.shade200,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "01:56",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Text(
+                  "03:00",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  ]);
+}
+
+Widget recommendations(context) {
+  return Column(children: [
+    ...List.generate(4, (index) {
+      return Card(
+        color: Colors.white,
+        elevation: 0.1,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/breath_$index.png',
+                  width: 50,
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Focusing the mind",
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      "${index + 4} mins",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.play_arrow),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    })
+  ]);
 }
